ec7e84903cd3d68c9ab062370f220f0f42c0afde
Shaakir <abdishakurdayib70@gmail.com>
2025-04-06 05:00:52 +0300

    version 1

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vite.dev/config/
+// change this to your repo name!
+const repoName = 'xafladia'
+
 export default defineConfig({
+  base: `/${repoName}/`, // 👈 this is for GitHub Pages!
   plugins: [vue()],
   resolve: {
     alias: {
